2d0746bc5ffb89acd6e55cc947c53e945ddf912d
matheus <mattmvc56@gmail.com>
2022-06-20 14:55:36 -0400

    feat: add step indicator close button visible

--- lib/src/modules/set_money/controller/set_money_controller.dart
+++ lib/src/modules/set_money/controller/set_money_controller.dart
@@ -4,9 +4,26 @@ class SetMoneyController {
   var indicator$ = ValueNotifier(34);
   var indexPage$ = ValueNotifier(0);
   var hasVisible$ = ValueNotifier(false);
+
   int get indicator => indicator$.value;
   int get indexPage => indexPage$.value;
   bool get hasVisible => hasVisible$.value;
+
+  void incrementIndexPage() {
+    indexPage$.value++;
+    _incrementIndicator();
+    _hasVisible();
+  }
+
+  void decrementIndexPage(context) {
+    if (indicator > 66) {
+      indexPage$.value--;
+      _decrementIndicator();
+      _hasVisible();
+    } else {
+      Navigator.pop(context);
+    }
+  }
 
   void _hasVisible() {
     if (indicator$.value > 67) {
@@ -16,23 +33,6 @@ class SetMoneyController {
     }
   }
 
-  void incrementIndicator() => indicator$.value = indicator$.value + 33;
-
-  void incrementIndexPage() {
-    indexPage$.value++;
-    incrementIndicator();
-    _hasVisible();
-  }
-
-  void decrementIndicator() => indicator$.value = indicator$.value - 33;
-
-  void decrementIndexPage(context) {
-    if (indicator > 66) {
-      decrementIndicator();
-      _hasVisible();
-      indexPage$.value--;
-    } else {
-      Navigator.pop(context);
-    }
-  }
+  void _incrementIndicator() => indicator$.value = indicator$.value + 33;
+  void _decrementIndicator() => indicator$.value = indicator$.value - 33;
 }
