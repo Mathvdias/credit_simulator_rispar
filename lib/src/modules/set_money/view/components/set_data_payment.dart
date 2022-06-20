05e4364ab9d48be52b6d68145af69deff1296e61
matheus <mattmvc56@gmail.com>
2022-06-20 17:49:36 -0400

    feat: slider percent and parcel

--- lib/src/modules/set_money/view/components/set_data_payment.dart
+++ lib/src/modules/set_money/view/components/set_data_payment.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 
+import '../../../../app/text_info_pattern.dart';
+import '../../../../components/app_rispar.dart';
+import 'slider_component.dart';
+import 'slider_component_percent.dart';
+
 class SetDataPayment extends StatefulWidget {
   final Function onNext;
   const SetDataPayment({
@@ -14,9 +19,78 @@ class SetDataPayment extends StatefulWidget {
 class _SetDataPaymentState extends State<SetDataPayment> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: ElevatedButton(
-          onPressed: () => widget.onNext(), child: const Text("Continuar2")),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const TextInfoPattern(fontSize: 25, titleExtension: 'Valor escolhido'),
+        _textColorGreen('R\$25.000'),
+        const TextInfoPattern(
+            fontSize: 18,
+            title: 'Escolha a ',
+            titleExtension: 'quantidade de parcelas'),
+        const SliderComponent(
+          divisions: 3,
+          max: 12,
+          min: 3,
+        ),
+        const TextInfoPattern(
+            fontSize: 18,
+            title: 'Escolha o ',
+            titleExtension: 'percentual de garantia'),
+        const SliderComponentPercent(
+          divisions: 2,
+          max: 50,
+          min: 20,
+        ),
+        
+        _textColorGreen('Garantia protegida'),
+        _buttonNextWithoutWarranty(),
+        _buttonNextPage(),
+      ],
+    );
+  }
+
+  _buttonNextWithoutWarranty() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kPadding),
+      child: SizedBox(
+        width: double.maxFinite,
+        height: 45,
+        child: TextButton(
+            onPressed: () {},
+            child: const Text('Continuar sem garantia',
+                style: TextStyle(fontWeight: FontWeight.bold))),
+      ),
+    );
+  }
+
+  _buttonNextPage() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kPadding),
+      child: SizedBox(
+          width: double.maxFinite,
+          height: 45,
+          child: ElevatedButton(
+              onPressed: () => widget.onNext(),
+              child: const Text(
+                "Continuar",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ))),
+    );
+  }
+
+  _textColorGreen(String text) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: kPadding),
+      child: RichText(
+        text: TextSpan(
+          text: text,
+          style: const TextStyle(
+              color: Color(0xff559597),
+              fontSize: 25,
+              fontWeight: FontWeight.bold),
+        ),
+      ),
     );
   }
 }
---
+++ lib/src/modules/set_money/view/components/slider_component.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class SliderComponent extends StatefulWidget {
+  final double min;
+  final double max;
+  final int divisions;
+  const SliderComponent({
+    Key? key,
+    required this.min,
+    required this.max,
+    required this.divisions,
+  }) : super(key: key);
+
+  @override
+  State<SliderComponent> createState() => _SliderComponentState();
+}
+
+class _SliderComponentState extends State<SliderComponent> {
+  double _currentSliderValue = 3;
+  @override
+  Widget build(BuildContext context) {
+    return SliderTheme(
+      data: SliderTheme.of(context).copyWith(
+        trackShape: const RoundedRectSliderTrackShape(),
+        trackHeight: 5.0,
+        thumbShape: const RoundSliderThumbShape(enabledThumbRadius: 12.0),
+        overlayShape: const RoundSliderOverlayShape(overlayRadius: 32.0),
+        tickMarkShape: const RoundSliderTickMarkShape(),
+        valueIndicatorShape: const PaddleSliderValueIndicatorShape(),
+        valueIndicatorTextStyle: const TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      child: Slider(
+        value: _currentSliderValue,
+        min: widget.min,
+        max: widget.max,
+        divisions: widget.divisions,
+        label: _currentSliderValue.round().toString(),
+        onChanged: (double values) {
+          setState(() {
+            _currentSliderValue = values;
+          });
+        },
+      ),
+    );
+  }
+}
